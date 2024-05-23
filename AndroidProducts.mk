7378f4915311d09bcab19211eed0025ae3c4139e
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-05-24 01:24:22 +0700

    even: sync

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_even.mk
+    $(LOCAL_DIR)/cipher_even.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_even-eng \
-    lineage_even-userdebug \
-    lineage_even-user
+    cipher_even-eng \
+    cipher_even-userdebug \
+    cipher_even-user
