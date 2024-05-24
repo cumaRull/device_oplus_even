0a725a4c3c4153e86937daa12c6b6103c353d9f5
CumaRull <102359107+cumaRull@users.noreply.github.com>
2024-05-24 08:12:00 +0700

    Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -32,4 +32,5 @@ def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
+  AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor")
   return
